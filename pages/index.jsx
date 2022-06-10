76ee17629c88892314822bf1738cc0538d93d865
pulgueta <retardix456@gmail.com>
2022-06-09 20:55:39 -0500

    colorMode

--- pages/index.jsx
+++ pages/index.jsx
@@ -24,7 +24,7 @@ const Home = () => {
         <meta name="author" content="Andrés Rodríguez (pulgueta)" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <Box width="100%" h="80px" boxShadow="md">
+      <Box width="100%" h="90px" boxShadow={isDark ? "md" : "base"}>
         <Flex
           maxW="1024px"
           px={[8, 12, 14, 16, 0]}
@@ -36,17 +36,18 @@ const Home = () => {
           <Heading
             userSelect="none"
             fontWeight="700"
-            fontSize={{ base: "32px" }}
+            fontSize={{ base: "28px", md: "32px" }}
           >
             Andrés Rodríguez
           </Heading>
           <IconButton
-            icon={isDark ? <MoonIcon /> : <SunIcon />}
+            icon={isDark ? <SunIcon /> : <MoonIcon />}
             onClick={toggleColorMode}
           ></IconButton>
         </Flex>
       </Box>
-      <Box h="calc(100vh - 80px)">
+
+      <Box h="calc(100vh - 90px)">
         <Flex
           h="100%"
           direction="column"
